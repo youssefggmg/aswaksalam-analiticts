16ab9fdd451095148831d338439e11f814df5194
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-04-21 16:11:28 +0100

    we added some changes

--- JS/loge-out.js
+++ JS/loge-out.js
@@ -1,6 +1,10 @@
 // select elements from the DOM
 let loge_out=document.querySelector("#loge-out")
 
+if (sessionStorage.getItem("conected")==null) {
+    window.location.href="loge in.html";
+}
+
 // add event listener to the logout button
 loge_out.addEventListener("click",(e)=>{
     window.location.href="loge in.html"
